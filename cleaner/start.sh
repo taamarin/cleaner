63279eb6a3a38dfb0075da493967fb1b935680c7
sheynsw <juwandafly@gmail.com>
2023-03-02 22:13:03 +0700

    optimize script

--- cleaner/start.sh
+++ cleaner/start.sh
@@ -1,19 +1,44 @@
 #!/system/bin/sh
 
+# Set the module directory based on the Magisk version
 moddir=/data/adb/modules/cache_cleaner
 if [ -n "$(magisk -v | grep lite)" ]; then
   moddir=/data/adb/lite_modules/cache_cleaner
 fi
+
+# Set the scripts directory
 scripts_dir=/data/adb/cleaner
 
+# Set the log file paths
+log_file=/data/adb/cleaner/run/cleaner.log
+service_log=/data/adb/cleaner/run/service.log
+
+# Check if the necessary directories exist
+if [ ! -d ${moddir} ] || [ ! -d ${scripts_dir} ]; then
+  echo "Error: Directories not found" >&2
+  exit 1
+fi
+
+# Check if inotifyd is installed
+if ! type inotifyd > /dev/null; then
+  echo "Error: inotifyd command not found" >&2
+  exit 1
+fi
+
+# Function to start the cleaner service
 start_cleaner() {
-  ${scripts_dir}/cleaner.service start
+  ${scripts_dir}/cleaner.service start > ${log_file} 2> ${service_log}
 }
 
-if [ ! -f /data/adb/cleaner/manual ] ; then
-  echo -n "" > /data/adb/cleaner/run/cleaner.log
+# If the manual file doesn't exist
+if [ ! -f ${scripts_dir}/manual ] ; then
+  # Clear the log file
+  echo -n "" > ${log_file}
+  # If the module is not disabled
   if [ ! -f ${moddir}/disable ] ; then
+    # Start the cleaner service
     start_cleaner
   fi
+  # Start inotifyd to monitor the module directory
   inotifyd ${scripts_dir}/cleaner.inotify ${moddir} &>> /dev/null &
 fi
--- cleaner_service.sh
+++ cleaner_service.sh
@@ -1,8 +1,20 @@
 #!/system/bin/sh
 
+# Set the path to the service script
+service_script=/data/adb/cleaner/start.sh
+
+# Check if the necessary directories and files exist
+if [ ! -f ${service_script} ]; then
+  echo "Error: Service script not found" >&2
+  exit 1
+fi
+
+# Wait for the system to finish booting
 (
 until [ $(getprop sys.boot_completed) -eq 1 ] ; do
   sleep 5
 done
-/data/adb/cleaner/start.sh
+
+# Launch the service script
+${service_script}
 )&
